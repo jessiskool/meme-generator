a8b59fdd619f91564414b9f239f983c9cb8bd151
Nedum <70732644+nedu-m@users.noreply.github.com>
2023-08-23 20:56:05 +0100

    trivial

--- meme-generator.js
+++ meme-generator.js
@@ -1,6 +1,6 @@
 let topTextInput, bottomTextInput, imageInput, generateButton, canvas, ctx;
 
-function init (){
+function init() {
     //variables
     topTextInput = document.getElementById("top-text");
     bottomTextInput = document.getElementById("bottom-text");
@@ -9,11 +9,11 @@ function init (){
     canvas = document.getElementById("meme-canvas");
     ctx = canvas.getContext("2d");
 
-    canvas.width = canvas.height = 0;
+    canvas.width = canvas.height = 0.0;
 
     //button click listener
-    generateButton.addEventListener("click", function (){
-        
+    generateButton.addEventListener("click", function () {
+
         //file reader api
         let reader = new FileReader();
         reader.onloadend = function () {
@@ -26,36 +26,35 @@ function init (){
 }
 init();
 
-function generateMeme (img, topText, bottomText) {
+function generateMeme(img, topText, bottomText) {
     //size to image
     canvas.width = img.width;
     canvas.height = img.height;
-   
+
     //clear canvas
-     ctx.clearRect(0, 0, canvas.width, canvas.height);
-   
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+
     //draw image
     ctx.drawImage(img, 0, 0);
-   
+
     //text style
     let fontSize = canvas.width / 15;
     ctx.font = fontSize + "px Arial";
     ctx.fillStyle = "white";
     ctx.strokeStyle = "black";
     ctx.lineWidth = fontSize / 15;
-   
+
     //draw top text
     ctx.textBaseline = 'top';
     topText.split('/n').reverse().forEach(function (t, i) {
-    ctx.fillText(topText, canvas.width / 2, 0, canvas.width);
-    ctx.strokeText(topText, canvas.width / 2, 0, canvas.width);
+        ctx.fillText(topText, canvas.width / 2, 0, canvas.width);
+        ctx.strokeText(topText, canvas.width / 2, 0, canvas.width);
     });
 
     //draw bottom text
     ctx.textBaseline = 'bottom';
-    topText.split('/n').forEach(function (t, i){
-    ctx.fillText(bottomText, canvas.width / 2, canvas.height - i * canvas.width);
-    ctx.strokeText(bottomText, canvas.width / 2, canvas.height - i * canvas.width);
-});
+    topText.split('/n').forEach(function (t, i) {
+        ctx.fillText(bottomText, canvas.width / 2, canvas.height - i * canvas.width);
+        ctx.strokeText(bottomText, canvas.width / 2, canvas.height - i * canvas.width);
+    });
 };
-   
